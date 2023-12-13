4f544985dfb6a5247639114a1ba3732a0f766985
Dominik Kinkela <50632865+MrDuck128@users.noreply.github.com>
2023-12-13 21:29:14 +0100

    Day 13, Mirror which I haven't seen

--- 2023/13.py
+++ 2023/13.py
@@ -7,39 +7,23 @@ def checkGrid(grid, trans, diffs_allowed):
     R = len(grid)
     C = len(grid[0])
     for j in range(C-1):
-        middle = C // 2
+        delta = j if j < (C-1) // 2 else C - j - 2
         check = True
         diffs = 0
-
-        if j < middle:
-            for i in range(R):
-                if not diffs_allowed:
-                    if not np.array_equal(grid[i][:j+1], np.flip(grid[i][j+1:(j+1)*2])):
-                        check = False
-                        break
-                else:
-                    diffs += np.sum(grid[i][:j+1] != np.flip(grid[i][j+1:(j+1)*2]))
-                    if diffs > diffs_allowed:
-                        break
-            if diffs_allowed and diffs == diffs_allowed:
-                return (j+1)*100 if trans else j+1
-            if not diffs_allowed and check:
-                return (j+1)*100 if trans else j+1
-                
-        else:
-            for i in range(R):
-                if not diffs_allowed:
-                    if not np.array_equal(grid[i][(j+1)*2-C:j+1], np.flip(grid[i][j+1:])):
-                        check = False
-                        break
-                else:
-                    diffs += np.sum(grid[i][(j+1)*2-C:j+1] != np.flip(grid[i][j+1:]))
-                    if diffs > diffs_allowed:
-                        break
-            if diffs_allowed and diffs == diffs_allowed:
-                return (j+1)*100 if trans else j+1
-            if not diffs_allowed and check:
-                return (j+1)*100 if trans else j+1
+        
+        for r in range(R):
+            if not diffs_allowed:
+                if not np.array_equal(grid[r][j-delta:j+1], np.flip(grid[r][j+1:j+delta+2])):
+                    check = False
+                    break
+            else:
+                diffs += np.sum(grid[r][j-delta:j+1] != np.flip(grid[r][j+1:j+delta+2]))
+                if diffs > diffs_allowed:
+                    break
+        if diffs_allowed and diffs == diffs_allowed:
+            return (j+1)*100 if trans else j+1
+        if not diffs_allowed and check:
+            return (j+1)*100 if trans else j+1
     return 0
 
 total = total2 = 0
@@ -47,18 +31,12 @@ for grid in data:
     grid = np.array([[*line] for line in grid.splitlines()])
 
     t = checkGrid(grid, False, 0)
-    if t:
-        total += t
-        continue
-    total += checkGrid(grid.T, True, 0)
-print('Part 1:', total)
-
-for grid in data:
-    grid = np.array([[*line] for line in grid.splitlines()])
+    if t: total += t
+    else: total += checkGrid(grid.T, True, 0)
 
     t2 = checkGrid(grid, False, 1)
-    if t2:
-        total2 += t2
-        continue
-    total2 += checkGrid(grid.T, True, 1)
+    if t2: total2 += t2
+    else: total2 += checkGrid(grid.T, True, 1)
+
+print('Part 1:', total)
 print('Part 2:', total2)
