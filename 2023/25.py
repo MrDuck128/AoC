125670464a5b68385f513e93059bf710349c521b
Dominik Kinkela <50632865+MrDuck128@users.noreply.github.com>
2023-12-26 04:15:51 +0100

    Day 25, The end has arrived

---
+++ 2023/25.py
@@ -0,0 +1,18 @@
+import networkx as nx
+
+with open('25.txt') as f:
+    data = f.read().splitlines()
+
+graph = nx.Graph()
+for line in data:
+    left, right = line.split(': ')
+    for node in right.split():
+        graph.add_edge(left, node, capacity=1.0)
+
+node1 = list(graph.nodes)[0]
+for node in graph.nodes:
+    if node != node1:
+        cut, (a, b) = nx.minimum_cut(graph, node, node1)
+        if cut == 3:
+            print(len(a) * len(b))
+            break
