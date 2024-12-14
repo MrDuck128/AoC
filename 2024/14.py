ca8c109bab82d61adc7b179b40fccd1384e54df6
MrDuck128 <dominikkinkela@gmail.com>
2024-12-14 12:07:35 +0100

    Day 14, preem

---
+++ 2024/14.py
@@ -0,0 +1,45 @@
+import re
+
+with open('14.txt') as f:
+    lines = f.read().splitlines()
+
+robots = {(d, c, b, a): (b, a) for line in lines for a, b, c, d in [map(int, re.findall('-?\\d+', line))]}
+h, w = 103, 101
+
+def advanceRobots(robots):
+    for (di, dj, *rest), (i, j) in robots.items():
+        newI = (i+di)%h if (i+di) > -1 else h+(i+di)
+        newJ = (j+dj)%w if (j+dj) > -1 else w+(j+dj)
+        robots[(di, dj, *rest)] = (newI, newJ)
+    return robots
+
+for sec in range(100):
+    robots = advanceRobots(robots)
+
+q1 = q2 = q3 = q4 = 0
+hh, hw = h//2, w//2
+for (i, j) in robots.values():
+    if i == hh or j == hw: continue
+    if i < hh:
+        if j < hw: q1 += 1
+        else: q2 += 1
+    else:
+        if j < hw: q3 += 1
+        else: q4 += 1
+
+print('Part 1:', q1 * q2 * q3 * q4)
+
+def checkVertical(coords):
+    l = 0
+    coords = sorted(coords, key=lambda x: (x[1], x[0]))
+    for (i, j), (ni, nj) in zip(coords, coords[1:]):
+        if l > 10: return 1
+        if j == nj and -1 < ni-1 and i == ni-1: l += 1
+        else: l = 0
+    return 0
+
+for sec in range(101, 10_000):
+    robots = advanceRobots(robots)
+    if checkVertical(robots.values()): 
+        print('Part 2:', sec)
+        break
