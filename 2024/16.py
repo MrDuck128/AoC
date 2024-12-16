822bcff59e1e0874a5a19a0050e27cc180cf804a
Dominik Kinkela <50632865+MrDuck128@users.noreply.github.com>
2024-12-16 16:56:07 +0100

    Day 16, speed

--- 2024/16.py
+++ 2024/16.py
@@ -8,25 +8,22 @@ si, sj = [(i, j) for i in range(len(grid)) for j in range(len(grid[0])) if grid[
 dirs = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}
 
 Q = [(0, si, sj, 1, [(si, sj)])]
-fullPath = []
-seen = set()
+seen = {}
 bestScore = 0
 totalTiles = set()
 
 while Q:
     score, i, j, d, path = heappop(Q)
 
-    seen.add((i, j, d))
+    if (i, j, d) not in seen:
+        seen[(i, j, d)] = float('inf')
 
     if grid[i][j] == 'E':
         if not bestScore:
             print('Part 1:', score)
             bestScore = score
-            for (pi, pj) in path:
-                if (pi, pj) not in totalTiles:
-                    totalTiles.add((pi, pj))
 
-        elif score == bestScore:
+        if score == bestScore:
             for (pi, pj) in path:
                 if (pi, pj) not in totalTiles:
                     totalTiles.add((pi, pj))
@@ -34,7 +31,8 @@ while Q:
     di, dj = dirs[d]
 
     for (nscore, ni, nj, nd, path) in [(score+1, i+di, j+dj, d, path+[(i+di, j+dj)]), (score+1000, i, j, (d+1)%4, path), (score+1000, i, j, (d-1)%4, path)]:
-        if grid[ni][nj] == '#' or (ni, nj, nd) in seen: continue
+        if grid[ni][nj] == '#' or ((ni, nj, nd) in seen and seen[(ni, nj, nd)] <= nscore): continue
+        seen[(ni, nj, nd)] = nscore
         heappush(Q, (nscore, ni, nj, nd, path))
 
 print('Part 2:', len(totalTiles))
