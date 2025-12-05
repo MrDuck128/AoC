32d3894eb20a71dd41f780ace30e2285630ea9c2
MrDuck128 <dominikkinkela@gmail.com>
2025-12-05 20:40:14 +0100

    Zakasnina drugi PC

--- 2025/05.py
+++ 2025/05.py
@@ -7,7 +7,7 @@ print('Part 1:', sum(any(a <= ing <= b for a, b in ranges) for ing in ings))
 
 total, pb = 0, 0
 for a, b in sorted(ranges):
-    if b <= pb and a <= pb: continue
+    if b <= pb: continue
     elif a <= pb: total += b - pb
     else: total += b - a + 1
     pb = b
